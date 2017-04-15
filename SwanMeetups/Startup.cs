ac626175c77ef6af3fc411cb2a726b547bedae03
David Nesbitt <david.djinn2k@gmail.com>
2017-04-15 14:18:47 +0100

    Add project files.

---
+++ SwanMeetups/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SwanMeetups.Startup))]
+namespace SwanMeetups
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
